4239937e4eea3bef11bb941c0b5661a1de011e89
Ivo van Stokkum <18715155+ism200@users.noreply.github.com>
2023-10-13 15:42:23 +0200

    introduced 2nd oscillation shifted by alpha[0]

--- glotaran/builtin/megacomplexes/pfid/pfid_megacomplex.py
+++ glotaran/builtin/megacomplexes/pfid/pfid_megacomplex.py
@@ -235,22 +235,30 @@ def calculate_pfid_matrix_gaussian_irf(
     # where 0.03 is the product of speed of light 3*10**10 cm/s and time-unit ps (10^-12)
     # we postpone the conversion because the global axis is
     # always expected to be in cm-1 for relevant experiments
-    # frequency_diff = (global_axis_value - frequencies) * 0.03 * 2 * np.pi
-    frequency_diff = alpha[0] * (global_axis_value - frequencies) * 0.03 * 2 * np.pi
+    frequency_diff = (global_axis_value - frequencies) * 0.03 * 2 * np.pi
+    # 20230925
+    # attempt: alpha[0] is a shift of the frequencies to mimic inhomogeneous broadening
+    frequency_diff2 = (global_axis_value - frequencies-alpha[0]) * 0.03 * 2 * np.pi
+    # frequency_diff = alpha[0] * (global_axis_value - frequencies) * 0.03 * 2 * np.pi
     d = width**2
     k = rates + 1j * frequency_diff
     dk = k * d
+    k2 = rates + 1j * frequency_diff2
+    dk2 = k2 * d
     sqwidth = np.sqrt(2) * width
 
     a = np.zeros((len(model_axis), len(rates)), dtype=np.complex128)
+    a2 = np.zeros((len(model_axis), len(rates)), dtype=np.complex128)
     # a[np.ix_(right_shifted_axis_indices, pos_idx)] = np.exp(
     #     (-1 * right_shifted_axis[:, None] + 0.5 * dk[pos_idx]) * k[pos_idx]
     # )
 
     # a[np.ix_(left_shifted_axis_indices, neg_idx)] = np.exp(
     a = np.exp((-1 * shifted_axis[:, None] + 0.5 * dk[:]) * k[:])
+    a2 = np.exp((-1 * shifted_axis[:, None] + 0.5 * dk2[:]) * k2[:])
 
     b = np.zeros((len(model_axis), len(rates)), dtype=np.complex128)
+    b2 = np.zeros((len(model_axis), len(rates)), dtype=np.complex128)
     # b[np.ix_(right_shifted_axis_indices, pos_idx)] = 1 + erf(
     #     (right_shifted_axis[:, None] - dk[pos_idx]) / sqwidth
     # )
@@ -259,12 +267,15 @@ def calculate_pfid_matrix_gaussian_irf(
     # b[np.ix_(True, neg_idx)] = 1 + erf(
     # b[np.ix_(neg_idx)] = 1 + erf(
     b = 1 + erf((shifted_axis[:, None] - dk[:]) / -sqwidth)
+    b2 = 1 + erf((shifted_axis[:, None] - dk2[:]) / -sqwidth)
     c = np.zeros((len(model_axis), len(rates)), dtype=np.complex128)
     c = 1 - erf((shifted_axis[:, None]) / -sqwidth)
     # added a minus to facilitate the NNLS fit of the instantaneous bleach
     osc = -(a * b + c) * scale
+    osc2 = -(a2 * b2 + c) * scale
     # output = np.zeros((len(model_axis), len(rates)), dtype=np.float64)
     output = (osc.real * rates - frequency_diff * osc.imag) / (rates**2 + frequency_diff**2)
+    output = output+(osc2.real * rates - frequency_diff2 * osc2.imag) / (rates**2 + frequency_diff2**2)
     # output = (osc.real * alpha[0] * rates - frequency_diff * osc.imag) / (
     #     (alpha[0] * rates) ** 2 + frequency_diff**2
     # )
